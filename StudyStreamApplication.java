5aaaa84df0a6a425b999d2bb5085d7351f129a54
Sakshi Ovhal <158707979+SKOvhal@users.noreply.github.com>
2025-07-14 13:33:03 +0530

    Add files via upload

---
+++ StudyStreamApplication.java
@@ -0,0 +1,16 @@
+package com.tka.studystream.api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyStreamApplication.class, args);
+		System.out.println("Application is Started...");
+		
+	}
+
+}
